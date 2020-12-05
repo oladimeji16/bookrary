1c8b39f50e7ce4659d31ad034b81af350f3e6507
Oladimeji Mustapha <49591168+oladimeji16@users.noreply.github.com>
2020-12-05 19:12:05 +0100

    Created Author Routes

--- server.js
+++ server.js
@@ -2,36 +2,38 @@ if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
 }
 
+//set up
 const express = require('express')
 const app = express()
 const expressLayouts= require('express-ejs-layouts')
 const mongoose = require('mongoose')
-mongoose.connect(process.env.DATANASE_URL, { useNewUrlParser: true })
+const bodyParser = require('body-parser')
+
+//Connect to mongoose
+mongoose.connect(process.env.DATANASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
 const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('Coonected to mongoDB'))
 
-const indexRou = require('./routes/index')
-
-
-
+//set up files
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({limit: '10mb', extended: false}))
 
 
-app.use('/', indexRou)
+//for Routes
+const indexRouter = require('./routes/index')
+const authorsRouter = require('./routes/authors')
+
+app.use('/', indexRouter)
+app.use('/authors', authorsRouter)
 
 
 
-
-
-
-
-
-
+//set up ports
 let port = process.env.PORT;
 if (port == null || port == "") {
     port = 3000;
